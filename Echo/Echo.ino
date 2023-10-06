ed946bc23af9c4eb2070bb8b702fe092e6e5679c
bioyakor <yakor@work>
2023-10-06 16:23:48 +0300

    edit ino

--- Echo/Echo.ino
+++ Echo/Echo.ino
@@ -16,10 +16,9 @@ enum ASP : uint8_t {
 };
 TUVK type_uvk;
 ASP type_asp;
-TUVK type_uvk_last;
-ASP type_asp_last;
 bool _is_ok;
 int last_read;
+bool btn_pressed;
 
 void GetUvkAsp(int analog) {
   switch (type_uvk) {
@@ -73,77 +72,92 @@ void GetUvkAsp(int analog) {
     type_uvk = SU25;
     type_asp = UN;
   }
-  if (type_uvk_last != type_uvk) {
-    digitalWrite(0, LOW);
-  } else if (type_asp_last != type_asp){ //&& type_asp != UN) {
-    digitalWrite(0, LOW);
-  }
 }
 
 void LocalDelay(int milsec) {
   //сюда запиздяшить опрос кнопки
   unsigned long last = millis();
+  int helper = -1;
   while (milsec > 0) {
     unsigned long now = millis();
     milsec -= now - last;
     last = now;
-    last_read = analogRead(1);
-    GetUvkAsp(last_read);
+    helper = analogRead(1);
+    // DigiUSB.print("LAST");
+    // DigiUSB.println(last_read, HEX);
+    // DigiUSB.delay(500);
+    // DigiUSB.print("HELP");
+    // DigiUSB.println(helper, HEX);
+    // DigiUSB.delay(500);
+    if ((last_read + 10 >= helper) || (helper >= last_read - 10)) {
+      if (!btn_pressed) {    //проверим есть ли нажатая уже.. необработанная. если нету будем менять значение
+        btn_pressed = true;  //взвели флаг нажатой кнопки
+        DigiUSB.println("CHANGE");
+        last_read = helper;  //на отпрвку
+        digitalWrite(0, LOW);
+      }
+    }
     DigiUSB.refresh();
-    //refresh();
   }
 }
+
+int ii;
 
 void setup() {
   pinMode(0, OUTPUT);
   digitalWrite(0, HIGH);
   type_uvk = UNKN;
   type_asp = UN;
-  type_uvk_last = UNKN;
-  type_asp_last = UN;
   last_read = 0;
+  btn_pressed = false;
   analogReference(DEFAULT);
   DigiUSB.begin();
   _is_ok = false;
   while (DigiUSB.read() != -1) {
   }
+  last_read = analogRead(1);
+  ii = 0;
 }
 
 void loop() {
   while (DigiUSB.read() != HELLO_GET) {
-    //DigiUSB.delay(100);
+    ii++;
     LocalDelay(100);
+    DigiUSB.println(ii, DEC);
+    if (ii > 5) {
+      ii = 0;
+      if (btn_pressed) {
+        digitalWrite(0, HIGH);
+        btn_pressed = false;
+        DigiUSB.println("TIMEOUT");
+      }
+    }
     return;
   }
   DigiUSB.write(HELLO_SEND);
-  LocalDelay(300);
-  //DigiUSB.delay(300);
   //HENDSHAKE
   uint8_t out = 0;
   uint8_t read = 0;
+  GetUvkAsp(last_read);
   out = type_uvk | type_asp;
-
   DigiUSB.write(out);
+  digitalWrite(0, HIGH);
+  btn_pressed = false;
   for (int i = 0; i < 100; i++)  // 1sec
   {
-    //DigiUSB.delay(100);
-    LocalDelay(100);
     read = DigiUSB.read();
     if (read == out) {
       _is_ok = true;
       break;
     }
+    LocalDelay(100);
   }
   if (_is_ok) {
-    //DigiUSB.delay(300);
-    LocalDelay(300);
     digitalWrite(0, HIGH);
   } else {  //TIMEOUT
     for (int i = 0; i < 3; i++) {
-      //DigiUSB.delay(100);
       LocalDelay(100);
       digitalWrite(0, LOW);
-      //DigiUSB.delay(100);
       LocalDelay(100);
       digitalWrite(0, HIGH);
     }
@@ -151,6 +165,4 @@ void loop() {
   //DigiUSB.delay(10);
   LocalDelay(10);
   _is_ok = false;
-  type_asp_last = type_asp;
-  type_uvk_last = type_uvk;
 }
